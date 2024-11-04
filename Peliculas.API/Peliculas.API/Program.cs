7780c52303d732bfb018e859a0cfff50fe035bdd
Victor Moreno <victor_moreno_rubio@hotmail.com>
2024-11-04 09:44:34 +0100

    error cargar jwt

--- Peliculas.API/Peliculas.API/Program.cs
+++ Peliculas.API/Peliculas.API/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         ValidateAudience = false,
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetConnectionString("jwtKey"))),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwtKey"])),
         ClockSkew = TimeSpan.Zero
     });
 
